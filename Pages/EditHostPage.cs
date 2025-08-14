c674aa3ec8436f9151a8efc5a167582889d389ed
Simon Hoxer Bønding <simon@hoxer.net>
2025-08-14 20:03:17 +0200

    Fixed EditHostPage clearing textboxes

--- Pages/EditHostPage.cs
+++ Pages/EditHostPage.cs
@@ -2,6 +2,9 @@ namespace ConsoleUI.Pages;
 
 public class EditHostPage : PageBase
 {
+    //Declare controls on the class to prevent being reset every render
+    TextBox txtHostName = new();
+    TextBox txtIpAddress = new();
     public EditHostPage() : base("Edit host")
     {
 
@@ -11,10 +14,17 @@ public class EditHostPage : PageBase
         ColumnContainer root = new();
         root.AddChild(new Label("Here comes a form to edit a host with"));
         root.AddChild(new Label("Enter host name"));
-        root.AddChild(new TextBox());
+        root.AddChild(txtHostName);
         root.AddChild(new Label("Enter IP address"));
-        root.AddChild(new TextBox());
-        root.AddChild(new Button("Go back", Navigation.Back));
+        root.AddChild(txtIpAddress);
+        RowContainer row = new();
+        row.AddChild(new Button("Go back", Navigation.Back));
+        row.AddChild(new Button("Reset", () => {
+            txtHostName.Content = "";
+            txtIpAddress.Content = "";
+        }));
+        root.AddChild(row);
+
         return root;
     }
 } //Pages/EditHostPage.cs
